799e797e5d09230878d886c0af3e8f3c3447229d
Antoine Briand <briand.antoine_jean_roger@courrier.uqam.ca>
2017-12-28 11:00:02 +0100

    Adding some spec for the TodoStore

--- src/tests/TodoStore.spec.ts
+++ src/tests/TodoStore.spec.ts
@@ -1,6 +1,60 @@
 import TodoStore from '../TodoStore'
 
-let Todo = new TodoStore()
+let TodoList: TodoStore = new TodoStore()
+
+beforeEach(() => {
+  TodoList = new TodoStore()
+})
+
 test('Initial todos list is empty', () => {
-  expect(Todo.todos).toEqual([])
+  expect(TodoList.todos).toEqual([])
+})
+
+test('The first call of increment() should return 1', () => {
+  expect(TodoStore.increment()).toBe(0)
+})
+
+test('The id of the first todo is 1', () => {
+  TodoList.addTodo('A todo')
+  expect(TodoList.todos[0].id).toBe(1)
+})
+
+test('The todos[] will be length 1 after adding the first todo', () => {
+  TodoList.addTodo('The first todo')
+  expect(TodoList.todos.length).toBe(1)
+})
+
+test('Removing the only todo will result of a todos lenght of 0', () => {
+  TodoList.addTodo('The first todo')
+  TodoList.removeTodo(TodoList.todos[0])
+  expect(TodoList.todos.length).toBe(0)
+})
+
+test('Updating title actually update it', () => {
+  TodoList.addTodo('The first todo')
+  TodoList.updateTitle(TodoList.todos[0], 'Brand new title')
+  expect(TodoList.todos[0].title).toEqual('Brand new title')
+})
+
+test('Any new task has a completed field set to false', () => {
+  TodoList.addTodo('The first todo')
+  TodoList.addTodo('The second todo')
+  expect(TodoList.todos[0].completed).toBe(false)
+  expect(TodoList.todos[1].completed).toBe(false)
+})
+
+test('toggleAll will set completed field to true to any todos', () => {
+  TodoList.addTodo('The first todo')
+  TodoList.addTodo('The second todo')
+  TodoList.toggleAll()
+  expect(TodoList.todos[0].completed).toBe(true)
+  expect(TodoList.todos[1].completed).toBe(true)
+})
+
+test('clearCompleted() will remove any completed todo', () => {
+  TodoList.addTodo('The first todo')
+  TodoList.addTodo('The second todo')
+  TodoList.toggleAll()
+  TodoList.clearCompleted()
+  expect(TodoList.todos.length).toBe(0)
 })
